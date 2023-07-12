e6c1cd7b69708c60080b2fe6cd39d76656198feb
visteen <visteen192@gmail.com>
2023-07-12 00:47:34 +0000

    dashboard controller added

---
+++ src/main/java/com/example/pharmacy_management_system/DashboardController.java
@@ -0,0 +1,88 @@
+package com.example.pharmacy_management_system;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class DashboardController implements Initializable {
+    @FXML
+    private Button add_medicine_btn;
+
+    @FXML
+    private Button add_medicine_btn_1;
+
+    @FXML
+    private Button add_medicine_btn_2;
+
+    @FXML
+    private Button add_medicine_btn_3;
+
+    @FXML
+    private Button add_supplier_btn;
+
+    @FXML
+    private Button add_supplier_btn_1;
+
+    @FXML
+    private Button add_supplier_btn_2;
+
+    @FXML
+    private Button add_supplier_btn_3;
+
+    @FXML
+    private Button dashboard_btn;
+
+    @FXML
+    private Button dashboard_btn_1;
+
+    @FXML
+    private Button dashboard_btn_2;
+
+    @FXML
+    private Button dashboard_btn_3;
+
+    @FXML
+    private AnchorPane dashboard_form;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private Button logout_btn_1;
+
+    @FXML
+    private Button logout_btn_2;
+
+    @FXML
+    private Button logout_btn_3;
+
+    @FXML
+    private AnchorPane medicine_form;
+
+    @FXML
+    private Button sell_medicine_btn;
+
+    @FXML
+    private Button sell_medicine_btn_1;
+
+    @FXML
+    private Button sell_medicine_btn_2;
+
+    @FXML
+    private Button sell_medicine_btn_3;
+
+    @FXML
+    private AnchorPane sell_medicine_form;
+
+    @FXML
+    private AnchorPane suplier_form;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
