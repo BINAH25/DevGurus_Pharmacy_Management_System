661c359192b8d966e96df63e4e3d9758a4c4a703
Emmanuel Ofori <eofori080@st.ug.edu.gh>
2023-07-26 23:12:07 +0000

    Names and Id's Added: project Manager

--- src/main/java/com/example/pharmacy_management_system/HelloApplication.java
+++ src/main/java/com/example/pharmacy_management_system/HelloApplication.java
@@ -1,5 +1,19 @@
 package com.example.pharmacy_management_system;
 
+/*
+
+    NAME                    -   IDS
+* Ernestine Akusika Dornyo  - 10923915
+
+  Ofori Emmanuel            - 10923044
+
+  Safura Aziz               - 10906101
+
+  Ahlijah Bernard Delalorm  - 10891195
+
+  Louis Binah               - 10899830
+*
+* */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
